d4307309079256d84650dfed5d678a16e0ec663f
srivtx <crypticcc101@gmail.com>
2026-08-20 21:59:37 +0530

    Keep only the published site (remove source files from pages branch)

--- programs/pre-req-vault/src/instructions.rs
+++
@@ -1,9 +0,0 @@
-pub mod close;
-pub mod deposit;
-pub mod initialize;
-pub mod withdraw;
-
-pub use close::*;
-pub use deposit::*;
-pub use initialize::*;
-pub use withdraw::*;
--- programs/pre-req-vault/src/state.rs
+++
@@ -1,8 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[derive(InitSpace)]
-#[account]
-pub struct VaultState {
-    pub vault_bump: u8,
-    pub state_bump: u8,
-}
